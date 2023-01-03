ca30f5ea4dcc0a0d12cd1385d3c7bee80d76566e
Michał Bogdziewicz <michal@bogdziewicz.xyz>
2023-01-03 20:43:15 +0100

    feat: Refactor audio sink

--- src/VitaAudioSink.cpp
+++ src/VitaAudioSink.cpp
@@ -6,27 +6,26 @@
 #include <psp2/appmgr.h>
 #include <cstring>
 
-#define BUFFER_SIZE       2048
+#define BUFFER_SIZE       3840
 #define VITA_DECODE_SIZE (BUFFER_SIZE / 4)
-#define BUFFER_COUNT     16
 
-static uint8_t g_buffer[BUFFER_COUNT][BUFFER_SIZE];
-static uint32_t data_len[BUFFER_COUNT] = {0};
-static int end_flag = 0;
-static int read_buffer = 0;
-static int write_buffer = 0;
+static uint8_t g_buffer[2][BUFFER_SIZE];
+static uint32_t data_len[2] = {0};
+static uint8_t end_flag = 0;
+static uint8_t read_buffer = 0;
+static uint8_t write_buffer = 0;
 static int port;
 
 static int feedBlocking() {
     while (end_flag == 0) {
         if (data_len[read_buffer] == BUFFER_SIZE) {
-            sceAudioOutOutput(port, g_buffer[read_buffer]);
+            int res = sceAudioOutOutput(port, g_buffer[read_buffer]);
+            if (res < 0) {
+                 CSPOT_LOG(error, "sceAudioOutOutput error");
+            }
             data_len[read_buffer] = 0;
 
-            read_buffer++;
-            if (read_buffer >= BUFFER_COUNT) read_buffer = 0;
-        } else {
-            sceKernelDelayThread(50000);
+            read_buffer = (read_buffer ? 0:1);
         }
     }
     return 0;
@@ -56,34 +55,17 @@ void VitaAudioSink::feedPCMFrames(const uint8_t *buffer, size_t bytes) {
         if (data_len[write_buffer] < BUFFER_SIZE) {
             uint32_t buffer_space = BUFFER_SIZE - data_len[write_buffer];
             uint32_t aval_data = bytes - pos;
-            uint32_t to_write;
-            if (aval_data > buffer_space) {
-                to_write = buffer_space;
-            } else {
-                to_write = aval_data;
-            }
+            uint32_t to_write = std::min(buffer_space, aval_data);
 
             memcpy(g_buffer[write_buffer]+data_len[write_buffer], buffer+pos, to_write);
             pos += to_write;
 
-            // set buffer length after making a copy to ensure this
-            // value won't be changed from another thread during comparison
-            uint32_t buffer_len = data_len[write_buffer] + to_write;
-            data_len[write_buffer] = buffer_len;
+            data_len[write_buffer] += to_write;
 
-            if (buffer_len == BUFFER_SIZE) {
-                write_buffer++;
-                if (write_buffer >= BUFFER_COUNT) write_buffer = 0;
+            if (data_len[write_buffer] == BUFFER_SIZE) {
+                write_buffer = (write_buffer ? 0:1);
             }
-
-            if (buffer_len > BUFFER_SIZE) {
-                CSPOT_LOG(error, "buffer overflow");
-            }
-
-        } else {
-            // CSPOT_LOG(debug, "buffer[%d] len: %d", write_buffer, data_len[write_buffer]);
-            sceKernelDelayThread(50000);
-        }
+        } 
     }
 }
 
